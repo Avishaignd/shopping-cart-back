d0652c9979c72fb28d9d8a2df5bb2f02743ee847
Avishaignd <avishaignd@gmail.com>
2021-08-02 10:11:52 +0300

    changes for deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const authRouter = require('./routes/authRouter')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://shopping-cart-front.netlify.app');
   res.header('Access-Control-Expose-Headers','Authorization');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
